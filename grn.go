a0de747f8fe1af16f7eb565cf0f6456c34e9ed67
susumu.yata <susumu.yata@gmail.com>
2015-06-19 15:20:25 +0900

    Remove gnx.go.
    GitHub: #1

--- grn.go
+++ grn.go
@@ -8,10 +8,66 @@ import "C"
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 	"strings"
 	"unsafe"
 )
+
+// -- Data types --
+
+type Bool uint8
+type Int int64
+type Float float64
+type GeoPoint struct{ Latitude, Longitude int32 }
+type Text []byte
+
+//type BoolVector []Bool
+//type IntVector []Int
+//type FloatVector []Float
+//type GeoPointVector []GeoPoint
+//type TextVector []Text
+
+const (
+	True  = Bool(3)
+	False = Bool(0)
+)
+
+func NullBool() Bool         { return Bool(1) }
+func NullInt() Int           { return Int(math.MinInt64) }
+func NullFloat() Float       { return Float(math.NaN()) }
+func NullGeoPoint() GeoPoint { return GeoPoint{math.MinInt32, math.MinInt32} }
+func NullText() Text         { return nil }
+
+type TypeID int
+
+const (
+	VoidID = TypeID(iota)
+	BoolID
+	IntID
+	FloatID
+	GeoPointID
+	TextID
+)
+
+func (id TypeID) String() string {
+	switch id {
+	case VoidID:
+		return "Void"
+	case BoolID:
+		return "Bool"
+	case IntID:
+		return "Int"
+	case FloatID:
+		return "Float"
+	case GeoPointID:
+		return "GeoPoint"
+	case TextID:
+		return "Text"
+	default:
+		return fmt.Sprintf("TypeID(%d)", id)
+	}
+}
 
 // -- Groonga --
 
