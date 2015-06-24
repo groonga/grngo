e6ec386b62bfb82eca578de57903aee2feb16efb
susumu.yata <susumu.yata@gmail.com>
2015-06-24 15:42:39 +0900

    Remove a comment about data type relationships.
    It was just a duplicate.
    
    GitHub: #8

--- grngo.go
+++ grngo.go
@@ -29,17 +29,8 @@ const NilID = uint32(C.GRN_ID_NIL)
 // See http://groonga.org/docs/reference/types.html for details.
 type DataType int
 
-// Relationships among Groonga and Golang built-in data types are as follows:
-//   ID: uint32
-//   Bool: bool
-//   (U)Int8/16/32/64: int64
-//   Float: float64
-//   Time: int64
-//   WGS84/TokyoGeoPoint: GeoPoint
-//   (Short/Long)Text: []byte
-//
-// Note that Time (int64) represents the number of microseconds elapsed since
-// the Unix epoch.
+// Time (int64) represents the number of microseconds elapsed since the Unix
+// epoch.
 //
 // See http://groonga.org/docs/reference/types.html for details.
 const (
